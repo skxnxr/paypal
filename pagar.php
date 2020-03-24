f46df603fa77cf1f93d55ecdce977e63157aee91
Skyner <sacwks@gmailcom>
2020-03-24 14:13:55 -0400

    Finalizado el video 414

--- pagar.php
+++ pagar.php
@@ -4,6 +4,8 @@ if (!isset($_POST['producto'], $_POST['precio'])) {
     exit("Hubo un error");
 }
 
+use PayPal\Api\Payer;
+
 // echo "<pre>";
 // var_dump($_POST);
 // echo "</pre>";
@@ -13,6 +15,11 @@ require 'config.php';
 $producto = htmlspecialchars($_POST['producto']);
 $precio = htmlspecialchars($_POST['precio']);
 $precio = (int) $precio;
-var_dump($precio);
+//var_dump($precio);
 $envio = 0;
 $total = $precio + $envio;
+
+$compra = new Payer();
+$compra->setPaymentMethod('paypal');
+
+echo $compra->getPaymentMethod();
